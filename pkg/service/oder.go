4c65c17fac59699fb5f1539be4a5420dbf7acdc6
oppolise <oppolise@gmail.com>
2025-09-30 22:17:18 +0700

    Implement order service interface and methods for order retrieval

--- pkg/service/oder.go
+++ pkg/service/oder.go
@@ -1,6 +1,35 @@
 package service
 
-type OrderService struct {
-	OrderId    int
-	OrderPrice int
+import (
+	"goPromotion/dto"
+	"goPromotion/pkg/repository"
+)
+
+type OrderItfService interface {
+	GetServiceOrder(uint) (*dto.OrderResponse, error)
+}
+
+type orderImpService struct {
+	orderRepo repository.OrderIrfRepository
+}
+
+func NewOrderImpService(orderRepo repository.OrderIrfRepository) OrderItfService {
+	return &orderImpService{orderRepo: orderRepo}
+}
+
+func (o *orderImpService) GetServiceOrder(id uint) (*dto.OrderResponse, error) {
+	order, err := o.orderRepo.GetPepoOrderByID(id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	oderResponse := dto.OrderResponse{
+		OrderID:  order.ID,
+		Price:    order.TotalPrice,
+		Discount: order.TotalDiscount,
+		NetPrice: order.NetPrice,
+	}
+
+	return &oderResponse, nil
 }
