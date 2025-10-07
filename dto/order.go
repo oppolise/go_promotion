9f9d64078b2be2607905e1b85d0548b6620377c1
oppolise <oppolise@gmail.com>
2025-10-07 20:48:46 +0700

    Add PromotionResult struct to DTO for enhanced order promotion details

--- dto/order.go
+++ dto/order.go
@@ -6,3 +6,16 @@ type OrderResponse struct {
 	Discount uint `json:"discount"`
 	NetPrice uint `json:"netPrice"`
 }
+
+type PromotionResult struct {
+	OrderID           uint            `json:"order_id"`
+	AppliedPromos     []uint          `json:"applied_promos"`
+	RejectedReasons   map[uint]string `json:"rejected_reasons"`
+	LineDiscounts     map[uint]int    `json:"line_discounts"`
+	OriginalSubtotal  int             `json:"original_subtotal"`
+	TotalItemDiscount int             `json:"total_item_discount"`
+	SubtotalAfterItem int             `json:"subtotal_after_item"`
+	TotalCartDiscount int             `json:"total_cart_discount"`
+	TotalDiscount     int             `json:"total_discount"`
+	FinalNetPrice     int             `json:"final_net_price"`
+}
